cfd48ae5570eaeac7ef3104ee23c751e9f4c23c5
Nihila Pallath <22f2001240@ds.study.iitm.ac.in>
2024-10-30 19:33:44 +0530

    styling correction and app route setup

--- backend/controller.py
+++ backend/controller.py
@@ -1,6 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask import current_app as app #to get the app which we defined in the main folder
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route('/login',methods=["GET","POST"])
+def signin():
+    if request.method=="POST":
+        uname=request.form.get('user_name')
+        pwd=request.form.get('password')
+    return render_template("login.html") #signup page is directing to login
+
+@app.route('/register')
+def signup():
+    return render_template("signup.html") #login is directing to signup
+
+# @app.route('/login_page')
+# def login():
+#     return render_template("user_dashboard.html")
